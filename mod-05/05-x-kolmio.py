87d86a389b56aa9245acdffaf60b265aaf62ee1a
Gruuno <jandda2000@gmail.com>
2026-08-28 11:37:53 +0300

    V - 1.0.1

---
+++ mod-05/05-x-kolmio.py
@@ -0,0 +1,11 @@
+korkeus = int(input("Anna kolmion korkeus!: "))
+
+rivi = 1
+
+while rivi <= korkeus:
+    välilyönnit = korkeus - rivi
+    tähdet = 2 * rivi - 1
+
+    print(" " * välilyönnit + "*" * tähdet)
+
+    rivi += 1
